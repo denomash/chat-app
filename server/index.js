0169f24c568b387c61738ee9f60caab48cd499f1
Dennis Macharia <dennismacharia4747@gmail.com>
2020-05-23 19:42:10 +0300

    add new client

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import socketio from "socket.io";
+import http from "http";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Listen to server
+server.listen(PORT, () => console.log(`Server running on locolhost:${PORT}`));
