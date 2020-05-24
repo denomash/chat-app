7e7cdfc4d29fdc55cf1352dd00ec299fd69ac50c
Dennis Macharia <dennismacharia4747@gmail.com>
2020-05-24 18:58:08 +0300

    add a socketio connection and disconnection functtion in the server Update join componet

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import socketio from "socket.io";
 import http from "http";
+import router from "./router";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", (socket) => {
+  console.log("We have a new connection!!!");
+
+  socket.on("disconnect", () => {
+    console.log("User has left!!!");
+  });
+});
+
+app.use(router);
 
 // Listen to server
 server.listen(PORT, () => console.log(`Server running on locolhost:${PORT}`));
