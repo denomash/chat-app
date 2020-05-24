b348b514f16b920d7a6049ae57adaaeeacae77a0
Dennis Macharia <dennismacharia4747@gmail.com>
2020-05-24 23:06:23 +0300

    user can send realtime messages add info header

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import io from "socket.io-client";
 
+import "./chat.css";
+import InfoBar from "../InfoBar/InfoBar";
+
 let socket;
 const ENDPOINT = "localhost:5000";
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
+  // Update on user join
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
@@ -24,7 +30,37 @@ const Chat = ({ location }) => {
       socket.off();
     };
   }, [ENDPOINT, location.search]);
-  return <h1>Chat Component...</h1>;
+
+  // Update messages
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  // function for sending messages
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
+
+  console.log("<<<<<", message, ">>>>>", messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        {/* <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => (e.key === "Enter" ? sendMessage(e) : null)}
+        /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import socketio from "socket.io";
 import http from "http";
 import router from "./router";
+import { addUser, removeUser, getUser, getUserInRoom } from "./users";
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,10 +11,33 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("We have a new connection!!!");
+  // Functionality when user joins a room
+  socket.on("join", ({ name, room }, callBack) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-  socket.on("join", ({ name, room}, callBack) => {
-    console.log(name, room);
+    if (error) return callBack(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+
+    socket.join(user.room);
+
+    callBack();
+  });
+
+  // Functionality when user sends a message
+  socket.on("sendMessage", (message, callBack) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callBack()
   });
 
   socket.on("disconnect", () => {
